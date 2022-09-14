d416d98aab1a8023f05c2b7a98b281e564c0f64a
Evastrings <elijahakandetea@gmail.com>
2022-09-14 07:14:16 -0700

    Task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,9 +7,9 @@ void times_table(void)
 {
 	int i, j, k;
 
-	for (i = 0; i < 9; i++)
+	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j < 9; j++)
+		for (j = 0; j < 10; j++)
 		{
 			k = i * j;
 			if (j == 0)
