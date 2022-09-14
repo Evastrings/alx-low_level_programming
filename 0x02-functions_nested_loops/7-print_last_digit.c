03e5989945ccd7f3fc5d59aade4d931fa78f2df5
Evastrings <elijahakandetea@gmail.com>
2022-09-13 22:34:52 -0700

    Task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_last_digit - Entry point
+ *  @n: the int to extract
+ *  Return: 0 success
+ */
+int print_last_digit(int n)
+{
+	int a;
+
+	if (n < 0)
+		n = -n;
+	a = n % 10;
+
+	if a < 0;
+	a = -a;
+
+	_putchar(a + 0);
+
+	return (a);
+}
