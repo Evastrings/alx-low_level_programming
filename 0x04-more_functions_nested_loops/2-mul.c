d0b308bedf611321bf23a70dc89599c52e96af59
Evastrings <elijahakandetea@gmail.com>
2022-09-15 09:52:34 -0700

    Task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - multuplies two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: product of a and b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
