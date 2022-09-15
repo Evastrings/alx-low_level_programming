ccc7f2f76d5601b181bdaa4686d3627c1653a98d
Evastrings <elijahakandetea@gmail.com>
2022-09-15 16:06:00 -0700

    Task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_most_numbers - print 0 - 9 except 2 & 4
+ */
+void print_most_numbers(void)
+{
+	int ev;
+
+	for (ev = 0; ev <= 9; ev++)
+	{
+		if (ev != 2 && ev != 4)
+			_putchar((ev % 10) + '0');
+	}
+	_putchar('\n')
+}
