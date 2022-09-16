e9e94ea40071146066d02a98b7e867277cbfc700
Evastrings <elijahakandetea@gmail.com>
2022-09-15 17:44:19 -0700

    Task 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * print_diagonal - draws a diagonal
+ * @n: numbers of character
+ */
+void print_diagonal(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i, j;
+
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < n; j++)
+			{
+				if (j == i)
+					_putchar('\\');
+				else if (j < i)
+
+					_putchar(' ');
+			}
+			_putchar('\n');
+		}
+	}
+}
