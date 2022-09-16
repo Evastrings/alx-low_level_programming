6322e4d15c6759aa3a1a8e2b8bfa0cc4a73f8465
Evastrings <elijahakandetea@gmail.com>
2022-09-15 17:16:06 -0700

    Task 05

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,11 +6,11 @@ void more_numbers(void)
 {
 	int x, y;
 
-	for (x = 0; x <= 9; x++)
+	for (x = 0; x < 10; x++)
 	{
 		for (y = 0; y <= 14; y++)
 		{
-			if (y < 9)
+			if (y >= 10)
 				_putchar((y / 10) + '0');
 			_putchar((y % 10) + '0');
 		}
