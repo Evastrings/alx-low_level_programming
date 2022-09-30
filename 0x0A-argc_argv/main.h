ea3e2a53afeedfffb251270f1776fa4d29431e5c
Evastrings <elijahakandetea@gmail.com>
2022-09-30 08:46:18 -0700

    0x0A

---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * File - main.h
+ * Description: Contains all prototypes to be used in this project
+ */
+
+int _putchar(char c);
+int _atoi(char *s);
+
+#endif /* MAIN_H */
