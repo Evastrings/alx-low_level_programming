c10d3da986343a75fb5991fe5f36b985d68f64a4
Evastrings <elijahakandetea@gmail.com>
2022-10-10 08:37:01 -0700

    Task 0

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,9 @@
+#ifndef OBJECT_LIKE_MACRO_H
+
+#define OBJECT_LIKE_MACRO_H
+/**
+ * SIZE an abbreviation for 1024
+ */
+#define SIZE 1024
+#endif
+
---
+++ 0x0D-preprocessor/README.md
@@ -0,0 +1,3 @@
+What are macros and how to use them
+What are the most common predefined macros
+How to include guard your header files
